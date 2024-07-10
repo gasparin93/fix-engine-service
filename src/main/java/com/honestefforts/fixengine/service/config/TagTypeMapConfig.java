381e1cdbc793bbd9ce489022b53902d8f311f475
Edgar Sias <eivansias@gmail.com>
2024-07-09 23:38:48 -0500

    Issue-5: adding types by tag config map, application.yml config file to inject from

---
+++ src/main/java/com/honestefforts/fixengine/service/config/TagTypeMapConfig.java
@@ -0,0 +1,24 @@
+package com.honestefforts.fixengine.service.config;
+
+import com.honestefforts.fixengine.model.validation.TagType;
+import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "tag.types")
+public class TagTypeMapConfig {
+  private final Map<String, TagType> typeMap;
+
+  public TagType getTypeOfTag(String tag) {
+    //TODO: there are a lot of STRING types, consider removing those and doing getOrDefault(STRING)
+    //for the time being, it's useful to keep track of supported tags
+    return typeMap.get(tag);
+  }
+}
