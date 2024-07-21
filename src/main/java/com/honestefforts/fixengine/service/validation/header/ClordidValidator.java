b6b6af5ba537b0db8a3ef1750b36c9e3ec3513fb
Edgar Sias <eivansias@gmail.com>
2024-07-21 15:43:29 -0500

    Issue-5: adding clordid validator that utilizes bloom filter using custom spring boot config injections

---
+++ src/main/java/com/honestefforts/fixengine/service/validation/header/ClordidValidator.java
@@ -0,0 +1,73 @@
+package com.honestefforts.fixengine.service.validation.header;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+import com.honestefforts.fixengine.model.message.tags.RawTag;
+import com.honestefforts.fixengine.model.validation.FixValidator;
+import com.honestefforts.fixengine.model.validation.ValidationError;
+import com.honestefforts.fixengine.service.config.ClordidValidationConfig;
+import com.honestefforts.fixengine.service.converter.util.CommonConversionUtil;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.locks.ReentrantLock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClordidValidator implements FixValidator {
+  private BloomFilter<String> bloomFilter;
+  private final ClordidValidationConfig clordidValidationConfig;
+  private ReentrantLock lock;
+  private LocalDate currentDay;
+
+  @Autowired
+  private ClordidValidator(ClordidValidationConfig clordidValidationConfig) {
+    this.clordidValidationConfig = clordidValidationConfig;
+    initializeBloomFilter();
+    this.currentDay = LocalDate.now();
+    this.lock = new ReentrantLock();
+  }
+
+  @Override
+  public ValidationError validate(final RawTag rawTag, final Map<String, RawTag> context) {
+    return Optional.ofNullable(context.get("60"))
+        .map(transactDtTag -> CommonConversionUtil.parseUtcTimestamp(transactDtTag.value()))
+        .map(LocalDateTime::toLocalDate)
+        .map(transactDate -> {
+          lock.lock();
+          try {
+            if (!transactDate.equals(currentDay)) {
+              initializeBloomFilter();
+              currentDay = transactDate;
+            }
+            if (bloomFilter.mightContain(rawTag.value())) {
+              //TODO: this may be a false positive - come up with a way to verify
+              //possible: db check - but this rules out elements being processed concurrently
+              return ValidationError.builder().submittedTag(rawTag).critical(true)
+                  .error("ClOrdID (tag 11) must be unique for a single trade date!")
+                  .build();
+            } else {
+              bloomFilter.put(rawTag.value());
+              return ValidationError.empty();
+            }
+          } finally {
+            lock.unlock();
+          }
+        })
+        .orElse(ValidationError.empty()); //transact date is invalid, that validator will yield critical error
+  }
+
+  private void initializeBloomFilter() {
+    bloomFilter = BloomFilter.create(Funnels.unencodedCharsFunnel(),
+        clordidValidationConfig.getExpectedInsertions(),
+        clordidValidationConfig.getFalsePositiveProbability());
+  }
+
+  @Override
+  public String supports() {
+    return "11";
+  }
+
+}
