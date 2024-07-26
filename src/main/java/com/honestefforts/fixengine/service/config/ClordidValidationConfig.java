a3d994f3b1384cf9f64e75f602476f10d96ec4e9
Edgar Sias <eivansias@gmail.com>
2024-07-26 14:21:31 -0500

    cleaning up testing code from config class

--- src/main/java/com/honestefforts/fixengine/service/config/ClordidValidationConfig.java
+++ src/main/java/com/honestefforts/fixengine/service/config/ClordidValidationConfig.java
@@ -1,22 +1,19 @@
 package com.honestefforts.fixengine.service.config;
 
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
+@Setter
+@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "validation.clordid")
 public class ClordidValidationConfig {
+  @NonNull
   private final double falsePositiveProbability;
+  @NonNull
   private final int expectedInsertions;
 
-  public ClordidValidationConfig(double falsePositiveProbability, int expectedInsertions) {
-    this.falsePositiveProbability = falsePositiveProbability;
-    this.expectedInsertions = expectedInsertions;
-  }
-
-  public double getFalsePositiveProbability() {
-    return falsePositiveProbability;
-  }
-
-  public int getExpectedInsertions() {
-    return expectedInsertions;
-  }
 }
--- src/main/java/com/honestefforts/fixengine/service/config/TagTypeMapConfig.java
+++ src/main/java/com/honestefforts/fixengine/service/config/TagTypeMapConfig.java
@@ -6,11 +6,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Component
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "tag")
 public class TagTypeMapConfig {
